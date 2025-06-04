67698e1243519df5ce8742b4e3c30fecb9a1bd40
renato.rodrigues-t1 <renato.rsufu@gmail.com>
2025-06-04 22:44:16 +0100

    Jest setup

--- angular-test/setup-jest.ts
+++ angular-test/setup-jest.ts
@@ -1,1 +1,31 @@
 import 'jest-preset-angular/setup-jest';
+import '@angular/localize/init';
+
+Object.defineProperty(URL, 'createObjectURL', {
+    value: jest.fn(),
+    writable: true
+});
+Object.defineProperty(URL, 'revokeObjectURL', {
+    value: jest.fn(),
+    writable: true
+});
+
+const mockResponse = jest.fn();
+Object.defineProperty(window, 'location', {
+    value: {
+        hash: {
+            endsWith: mockResponse,
+            includes: mockResponse
+        },
+        assign: mockResponse
+    },
+    writable: true
+});
+
+const mockIntersectionObserver = jest.fn();
+mockIntersectionObserver.mockReturnValue({
+    observe: () => null,
+    unobserve: () => null,
+    disconnect: () => null
+});
+window.IntersectionObserver = mockIntersectionObserver;
